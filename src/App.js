a9b37a5484df6be1eadaee3e852857e754d8bfa2
Fan Zheng <fan2015zheng@gmail.com>
2020-06-21 22:21:10 -0500

    add hover state tooltip and color change and zoom

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css'
 import Map from './Map'
 
 function App() {
-  return (<React.Fragment>
-    <Map />
-  </React.Fragment>);
+  
+  return (<>
+    <div className="container">
+      <h2 className="text-center">Covid-19 USA data</h2>
+      <Map />
+    </div>
+   
+  </>);
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -6,29 +6,44 @@ function Map() {
   const svgr = useRef()
 
   useEffect(() => {
-    d3.json("./usa.json").then(
-      (states) => {
-        const projection = d3.geoAlbersUsa()
-        const path = d3.geoPath().projection(projection)
-        
-        const svg = d3.select(svgr.current)
-
-        svg
-          .selectAll("path")
-          .data(states.features)
-          .enter()
-          .append("path")
-          .attr("d", path)
-      }
-    )
+    d3.json("./usa.json").then(DrawUSA)
   }, [])
 
-  return(<React.Fragment>
+  function DrawUSA(data) {
+    const projection = d3.geoAlbersUsa()
+    const path = d3.geoPath().projection(projection)
+
+    const svg = d3.select(svgr.current)
+    const g = svg.append("g")
+
+    svg
+      .call(d3.zoom().on('zoom', () => {
+        g.attr("transform", d3.event.transform)
+      }))
+   
+
+    g
+      .selectAll("path")
+      .data(data.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", "_statePath")
+      .append("title")
+      .text((d) => {
+        return d.properties.NAME
+      })
+  }
+
+  return(<>
     <svg ref={svgr} className="_svg" viewBox="80 0 800 500"></svg>
-  </React.Fragment>)
+  </>)
 }
 
 export default Map
+
+
+
 
 
 
