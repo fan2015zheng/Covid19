69e36806189acb0c288b4e49964b36a66c1c8db1
Fan Zheng <fan2015zheng@gmail.com>
2020-06-22 20:35:12 -0500

    show state covid most recent day data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Map from './Map'
+import Data from './Data'
 
 function App() {
   
+  const [stateId, setStateId] = useState()
+
+  function selectLocation (stateId) {
+    setStateId(parseInt(stateId))
+  }
+
   return (<>
+    <h2 className="text-center">Covid-19 USA data</h2>
     <div className="container">
       <div className="row">
-        <div className="col">
-          <h2 className="text-center">Covid-19 USA data</h2>
-          <Map />
+        <div className="col-sm">
+          <Map selectLocation={selectLocation}/>
+        </div>
+        <div className="col-sm">
+          <Data stateId={stateId}/>
         </div>
       </div>
     </div>
--- src/Map.js
+++ src/Map.js
@@ -2,38 +2,37 @@ import React, { useRef, useEffect } from 'react'
 import './Map.css'
 import * as d3 from 'd3'
 
-function Map() {
+function Map({selectLocation}) {
   const svgr = useRef()
-
-  useEffect(() => {
-    d3.json("./usa.json").then(DrawUSA)
-  }, [])
 
   function DrawUSA(data) {
     const projection = d3.geoAlbersUsa()
     const path = d3.geoPath().projection(projection)
 
     const svg = d3.select(svgr.current)
-    const g = svg.append("g")
-
     svg
-      .call(d3.zoom().on('zoom', () => {
-        g.attr("transform", d3.event.transform)
-      }))
-   
-
-    g
       .selectAll("path")
       .data(data.features)
       .enter()
       .append("path")
       .attr("d", path)
       .attr("class", "_statePath")
+      .on("click", (d) => {
+        
+        const stateId = d.properties.STATE
+        
+        selectLocation(stateId)
+      })
       .append("title")
       .text((d) => {
+        console.log(d.properties.GEO_ID,d.properties.STATE, d.properties.NAME)
         return d.properties.NAME
       })
   }
+
+  useEffect(() => {
+    d3.json("./usa.json").then(DrawUSA)
+  })
 
   return(<>
     <svg ref={svgr} className="_svg" viewBox="80 0 800 500"></svg>
